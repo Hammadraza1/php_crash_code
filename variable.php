a31c9e9f7f040f0cd6cf3d0f4199c3d520ceb5bd
hammadraza1 <ahmedhammadraza@gmail.com>
2024-04-04 00:37:32 +0530

    variable.php

---
+++ variable.php
@@ -0,0 +1,16 @@
+<?php
+$name = 'hammad';
+$age = 25;
+$work = 'software engineer';
+
+echo $name;
+echo $age;
+
+echo $name . ' is ' . $age;
+
+echo "$name is $age years old";
+
+echo '5' + '5';
+
+
+
